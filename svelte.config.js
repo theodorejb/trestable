fee79537a078f824c9a9d4011c378cba2e9d0b5d
Theodore Brown <theodorejb@outlook.com>
2023-06-06 15:38:30 -0500

    Use SPA mode for homepage

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: "index.html",
+        }),
+        prerender: { entries: [] },
+        paths: {
+            base: "/trestable",
+        },
     },
 };
 
